c9a1885e8e3f96ee02d7a7c6f5907e0db60fe779
SchwapTobi <ts.tobi.s@gmail.com>
2019-05-03 11:26:32 +0200

    added location data model, fetching geolocations for all cities

--- frontend/weatherNet/src/pages/home/home.ts
+++ frontend/weatherNet/src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+// @ts-ignore
+import * as datag from '../../assets/allcities.json';
+import {Location} from "../../model/position/location";
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +12,23 @@ import {NavController} from 'ionic-angular';
 
 export class HomePage {
 
+
   constructor(public navCtrl: NavController) {
+    this.process();
 
   }
 
+  private process() {
+    let allData = datag.features;
+    let allObjects = [];
+    let max = 10;
+
+    for (let i = 0; i < max; i++) {
+      console.log(allData[i]);
+      let obj = new Location(allData[i].zipCode.toString(), allData[i].name.toString());
+      allObjects.push(obj);
+    }
+
+    console.log(allObjects);
+  }
 }
